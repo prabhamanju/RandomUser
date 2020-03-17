32c445205019a6a7b5e6ff034615beb39387df0f
prabhamanju <manjuparbha@gmail.com>
2020-03-17 16:01:58 +0100

    random user app first version

---
+++ randomuserapp/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import DataFetching from './components/DataFetching';
+//import DeleteUser from './components/DeleteUser'
+function App() {
+  return (
+      <div className= 'App'>
+         <DataFetching /> 
+
+      </div>
+  );
+}
+
+export default App;
+
